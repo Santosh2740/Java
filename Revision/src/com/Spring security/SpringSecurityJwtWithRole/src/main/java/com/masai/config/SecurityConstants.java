885fff53475cc0385ea6a449cf28c2496460966c
Santosh2704 <santoshvishwa1572@gmail.com>
2023-02-25 11:23:32 +0530

    Completed!

---
+++ Revision/src/com/Spring security/SpringSecurityJwtWithRole/src/main/java/com/masai/config/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.masai.config;
+
+public interface SecurityConstants {
+
+	public static final String JWT_KEY ="https://jwt.io/#debugger-io?token=secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsdhttps://jwt.io/#debugger-io?token=secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
+	public static final String JWT_HEADER = "Authorization";
+	
+	
+}
